6daa36bc6da81774b2040e69dfe2de3327b5e89d
jazmine.villegas <jazminewrooman@hotmail.com>
2018-04-19 21:28:07 -0500

    Initial

---
+++ ExamenBanlinea/ExamenBanlineaPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ExamenBanlinea
+{
+    public partial class ExamenBanlineaPage : ContentPage
+    {
+        public ExamenBanlineaPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
